9d4fdb67669e3efe2661226c492e169bf734c7ee
Alex Zvoleff <azvoleff@gmail.com>
2014-05-13 11:26:44 -0400

    Update for latest teamlucc version.

--- test-cloud_remove.R
+++ test-cloud_remove.R
@@ -1,21 +1,5 @@
 context("cloud_remove")
 
-# Below is temporary - delete when done with direct testing
-# load_all('../../')
-# library(tools)
-# out_name <- NULL
-# use_IDL <- TRUE
-# fast <- FALSE
-# num_class <- 1
-# min_pixel <- 20
-# max_pixel <- 1000
-# cloud_nbh <- 1
-# DN_min <- 0
-# DN_max <- 255
-# idl <- "C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe"
-# patch_long <- 1000
-# verbose <- TRUE
-
 cloudy <- raster('cloud_remove/L20080724_cloudy')
 clear <- raster('cloud_remove/L20080606')
 cloud_mask <- raster('cloud_remove/cloud_mask')
@@ -23,47 +7,32 @@ cloud_mask <- raster('cloud_remove/cloud_mask')
 # Load reference image filled with Xiaolin's original code
 filled_ref <- raster('cloud_remove/img_cloudy_stack_filled_reference')
 
-out_name_idl <- 'cloud_remove/img_cloudy_stack_filled_idl'
-system.time(out_idl <- cloud_remove(cloudy, clear, cloud_mask, out_name_idl))
-
-out_name_idl_fast <- 'cloud_remove/img_cloudy_stack_filled_idl_fast'
-system.time(out_idl_fast <- cloud_remove(cloudy, clear, cloud_mask, 
-                                         out_name_idl_fast, fast=TRUE))
-
-out_name_r <- 'cloud_remove/img_cloudy_stack_filled_r'
-system.time(out_r <- cloud_remove(cloudy, clear, cloud_mask, out_name_r, 
-                                  use_IDL=FALSE, verbose=TRUE))
-
-# plot(clear[[1]])
-# plot(cloudy[[1]])
-# plot(cloud_mask[[1]])
-# plot(out_idl[[1]])
-# plot(out_r[[1]])
-# plot(out_idl_fast[[1]])
-# plot(filled_ref[[1]])
-#
-# mean(getValues(clear) - getValues(filled_ref))
-# mean(getValues(cloudy) - getValues(filled_ref))
-# expect_that(mean(getValues(out_r) - getValues(filled_ref))
-#
-# hist(filled_ref - out_idl)
-# hist(filled_ref - out_r)
-# hist(filled_ref - out_idl_fast)
-#
-# plotRGB(out_idl_fast, stretch="lin")
-# plotRGB(out_r, stretch="lin")
-# plotRGB(out_idl, stretch="lin")
-#
-# plot(out_idl[[1]] - filled_ref[[1]])
-# plot(out_idl_fast[[1]] - filled_ref[[1]])
-# plot(out_r[[1]] - filled_ref[[1]])
-
-# Approx 97 second run time
 test_that("input data is correct", {
     expect_less_than(abs(mean(getValues(clear) - getValues(cloudy))), 1.8)
 })
 
+# Approx 97 second run time
+out_idl_byblock <- cloud_remove(cloudy, clear, cloud_mask)
+# Approx 97 second run time
+out_idl <- cloud_remove(cloudy, clear, cloud_mask, byblock=FALSE)
+
 # Approx 17 second run time
+out_idl_fast_byblock <- cloud_remove(cloudy, clear, cloud_mask, fast=TRUE)
+# Approx 17 second run time
+out_idl_fast <- cloud_remove(cloudy, clear, cloud_mask, fast=TRUE, 
+                             byblock=FALSE)
+
+# Approx 22 second run time
+out_r_byblock <- cloud_remove(cloudy, clear, cloud_mask, use_IDL=FALSE)
+# Approx 22 second run time
+out_r <- cloud_remove(cloudy, clear, cloud_mask, use_IDL=FALSE, byblock=FALSE)
+
+test_that("byblock=FALSE and byblock=TRUE match for RasterLayers", {
+    expect_equivalent(getValues(out_idl), getValues(out_idl_byblock))
+    expect_equivalent(getValues(out_idl_fast), getValues(out_idl_fast_byblock))
+    expect_equivalent(getValues(out_r), getValues(out_r_byblock))
+})
+
 test_that("IDL cloud fill works correctly for RasterLayers", {
     expect_less_than(abs(mean(getValues(out_idl_fast) -
                               getValues(filled_ref))), .07)
@@ -71,42 +40,55 @@ test_that("IDL cloud fill works correctly for RasterLayers", {
                               getValues(filled_ref))), .02)
 })
 
-# Approx 22 second run time
 test_that("R cloud fill works correctly for RasterLayers", {
-    expect_less_than(abs(mean(getValues(out_r) -
-                              getValues(filled_ref))), .05)
+    expect_less_than(abs(mean(getValues(out_r) - getValues(filled_ref))), .11)
 })
 
+# Test code works when using out_names
+out_idl_outname <- cloud_remove(cloudy, clear, cloud_mask, 
+                                out_name=rasterTmpFile())
+out_idl_fast_outname <- cloud_remove(cloudy, clear, cloud_mask, fast=TRUE, 
+                                     out_name=rasterTmpFile())
+# Code for cloud_remove_R also needs to test outnames with and without byblock 
+# (not tested above as this code doesn't vary for cloud_remove_IDL).
+out_r_outname_byblock <- cloud_remove(cloudy, clear, cloud_mask, use_IDL=FALSE, 
+                                      out_name=rasterTmpFile())
+out_r_outname <- cloud_remove(cloudy, clear, cloud_mask, use_IDL=FALSE, 
+                              byblock=FALSE, out_name=rasterTmpFile())
+
+test_that("cloud_remove works when out_name is specified", {
+    expect_equivalent(getValues(out_idl), getValues(out_idl_outname))
+    expect_equivalent(getValues(out_idl_fast), getValues(out_idl_fast_outname))
+    expect_equivalent(getValues(out_r), getValues(out_r_outname))
+    expect_equivalent(getValues(out_r), getValues(out_r_byblock))
+})
 
 ###############################################################################
 # Repeat above tests, but run on a layer stack
 
 cloudy_stack <- stack(cloudy, cloudy, cloudy)
 clear_stack <- stack(clear, clear, clear)
-
-# Approx 120 second run time
-out_name_idl_stack <- 'cloud_remove/img_cloudy_stack_filled_idl'
-system.time(out_idl_stack <- cloud_remove(cloudy_stack, clear_stack, 
-                                          cloud_mask, out_name_idl_stack))
-
-# Approx 20 second run time
-out_name_idl_stack_fast <- 'cloud_remove/img_cloudy_stack_filled_idl_fast'
-system.time(out_idl_stack_fast <- cloud_remove(cloudy_stack, clear_stack, 
-                                               cloud_mask, 
-                                               out_name_idl_stack_fast, 
-                                               fast=TRUE))
-
-# Approx 23 second run time
-out_name_r_stack <- 'cloud_remove/img_cloudy_stack_filled_r'
-system.time(out_r_stack <- cloud_remove(cloudy_stack, clear_stack, cloud_mask, 
-                                        out_name_r_stack, use_IDL=FALSE, 
-                                        verbose=TRUE))
-
-plot(out_idl_stack[[1]])
-plot(out_idl_stack_fast[[1]])
-plot(out_r_stack[[1]])
-
 filled_ref_stack <- stack(filled_ref, filled_ref, filled_ref)
+
+# Approx 150 second run time
+out_idl_stack_byblock <- cloud_remove(cloudy_stack, clear_stack, cloud_mask)
+# Approx 150 second run time
+out_idl_stack <- cloud_remove(cloudy_stack, clear_stack, cloud_mask, 
+                              byblock=FALSE)
+
+# Approx 30 second run time
+out_idl_stack_fast_byblock <- cloud_remove(cloudy_stack, clear_stack, 
+                                           cloud_mask, fast=TRUE)
+# Approx 30 second run time
+out_idl_stack_fast <- cloud_remove(cloudy_stack, clear_stack, cloud_mask, 
+                                   fast=TRUE, byblock=FALSE)
+
+# Approx 30 second run time
+out_r_stack_byblock <- cloud_remove(cloudy_stack, clear_stack, cloud_mask, 
+                                    use_IDL=FALSE)
+# Approx 30 second run time
+out_r_stack <- cloud_remove(cloudy_stack, clear_stack, cloud_mask, 
+                            use_IDL=FALSE, byblock=FALSE)
 
 test_that("IDL cloud fill works correctly for RasterStack", {
     # IDL
@@ -127,9 +109,15 @@ test_that("IDL cloud fill works correctly for RasterStack", {
 
 test_that("R cloud fill works correctly for RasterStack", {
     expect_less_than(abs(mean(getValues(out_r_stack[[1]]) - 
-                              getValues(filled_ref_stack[[1]]))), .05)
+                              getValues(filled_ref_stack[[1]]))), .11)
     expect_less_than(abs(mean(getValues(out_r_stack[[2]]) - 
-                              getValues(filled_ref_stack[[2]]))), .05)
+                              getValues(filled_ref_stack[[2]]))), .11)
     expect_less_than(abs(mean(getValues(out_r_stack[[3]]) - 
-                              getValues(filled_ref_stack[[3]]))), .05)
+                              getValues(filled_ref_stack[[3]]))), .11)
+})
+
+test_that("byblock=FALSE and byblock=TRUE match for RasterStacks", {
+    expect_equivalent(out_idl_stack, out_idl_stack_byblock)
+    expect_equivalent(out_idl_stack_fast, out_idl_stack_fast_byblock)
+    expect_equivalent(out_r_stack, out_r_stack_byblock)
 })
--- test-fill_gaps.R
+++ test-fill_gaps.R
@@ -1,19 +1,5 @@
 context("fill_gaps")
 
-# Below are temporary - delete when done with direct testing
-# load_all('../../')
-# library(tools)
-# out_base <- NULL
-# use_IDL <- TRUE
-# sample_size <- 20
-# size_wind <- 12
-# class_num <- 4
-# DN_min <- 0.0
-# DN_max <- 1.0
-# patch_long <- 1000
-# idl <- "C:/Program Files/Exelis/IDL83/bin/bin.x86_64/idl.exe"
-# verbose <- TRUE
-
 slc_off <- brick('fill_gaps/TM20100429_toaR_gap')
 fill <- brick('fill_gaps/TM20100515_toaR')
 timeseries <- c(brick('fill_gaps/TM20100208_toaR'))
@@ -22,28 +8,23 @@ timeseries <- c(brick('fill_gaps/TM20100208_toaR'))
 filled_ref <- brick('fill_gaps/TM20100429_toaR_gap_GNSPI_reference')
 filled_ref_uncertainty <- brick('fill_gaps/TM20100429_toaR_gap_GNSPI_uncertainty_reference')
 
-# Approx 165 seconds
-out_base_no_timeseries <- 'fill_gaps/TM20100429_toaR_gap_no_timeseries'
-system.time(filled_no_timeseries <- fill_gaps(slc_off, fill, timeseries=c(), 
-                                              out_base=out_base_no_timeseries))
-
-# Approx 205 seconds
-out_base_with_timeseries <- 'fill_gaps/TM20100429_toaR_gap_with_timeseries'
-system.time(filled_with_timeseries <- fill_gaps(slc_off, fill, 
-                                                timeseries=timeseries, 
-                                                out_base=out_base_with_timeseries))
-
-# plot(slc_off[[1]])
-# plot(fill[[1]])
-# plot(filled_ref[[1]])
-# plot(filled_ref_uncertainty[[1]])
-# plot(filled_no_timeseries$filled[[1]] - filled_ref[[1]])
-# plot(filled_with_timeseries$filled[[1]] - filled_ref[[1]])
-
 test_that("input data is correct", {
     expect_less_than(abs(mean(getValues(slc_off) - getValues(filled_ref))), .05)
     expect_less_than(abs(mean(getValues(fill) - getValues(filled_ref))), .02)
 })
+
+# Approx 165 seconds
+filled_no_timeseries <- fill_gaps(slc_off, fill, timeseries=c())
+# Approx 165 seconds
+filled_no_timeseries_out_base <- fill_gaps(slc_off, fill, timeseries=c(), 
+                                           out_base=rasterTmpFile())
+
+# Approx 205 seconds
+filled_with_timeseries <- fill_gaps(slc_off, fill, timeseries=timeseries)
+# Approx 205 seconds
+filled_with_timeseries_out_base <- fill_gaps(slc_off, fill, 
+                                             timeseries=timeseries, 
+                                             out_base=rasterTmpFile())
 
 test_that("gap fill works properly", {
     expect_less_than(abs(mean(getValues(filled_no_timeseries$filled) - getValues(filled_ref))), 1e-4)
@@ -51,6 +32,19 @@ test_that("gap fill works properly", {
 })
 
 test_that("gap fill uncertainty works properly", {
-    expect_less_than(abs(mean(getValues(filled_no_timeseries$uncertainty) - getValues(filled_ref_uncertainty))), 1.07)
-    expect_less_than(abs(mean(getValues(filled_with_timeseries$uncertainty) - getValues(filled_ref_uncertainty))), .8)
+    expect_less_than(abs(mean(getValues(filled_no_timeseries$uncertainty) - 
+                              getValues(filled_ref_uncertainty))), 1.07)
+    expect_less_than(abs(mean(getValues(filled_with_timeseries$uncertainty) - 
+                              getValues(filled_ref_uncertainty))), .8)
+})
+
+test_that("gap fill works with out_base specified", {
+    expect_equivalent(filled_no_timeseries$filled, 
+                      filled_no_timeseries_out_base$filled)
+    expect_equivalent(filled_no_timeseries$uncertainty, 
+                      filled_no_timeseries_out_base$uncertainty)
+    expect_equivalent(filled_with_timeseries$filled, 
+                      filled_with_timeseries_out_base$filled)
+    expect_equivalent(filled_with_timeseries$uncertainty, 
+                      filled_with_timeseries_out_base$uncertainty)
 })
